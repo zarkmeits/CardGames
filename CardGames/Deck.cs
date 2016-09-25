9bb7b824166839e4f4e058d4b28e11b78ca9913e
Mark Zeits <zarkmeits@gmail.com>
2016-09-25 16:41:58 -0400

    Finished Deck Class v1 and Updated the Suit and Rank classes as needed.

---
+++ CardGames/Deck.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGames
+{
+    class Deck
+    {
+
+        int cardTotal;
+
+        public static List<Card> DECK;
+        public static List<Card> dealt;
+
+        public Deck()
+        {
+            DECK = new List<Card>();   
+            dealt = new List<Card>();
+        }
+
+        public void AddCard(Card c)
+        {
+            DECK.Add(c);    
+        }
+
+        public Card DealOne()
+        {
+            Card c = DECK[0];
+            dealt.Add(c);
+            DECK.RemoveAt(0);
+            return c;
+        }
+
+        public int GetCardsRemaining()
+        {
+            return DECK.Count;
+        }
+
+        public int GetDeckSize()
+        {
+            return (DECK.Count + dealt.Count);
+        }
+
+        public bool isEmpty()
+        {
+            if (DECK.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < DECK.Count; i++)
+            {
+                int rand = r.Next(0, DECK.Count);
+                Card temp = DECK[i];
+
+                DECK[i] = DECK[rand];
+                DECK[rand] = temp; 
+            }
+        }
+
+        public void RestoreDeck()
+        {
+            foreach(Card c in dealt)
+            {
+                DECK.Add(c);
+            }
+        }
+    }
+}
